c18feb8cebd7a9b77185c07c4a4ed90d5c3af00a
AsavaAsava <wayne.majani@hotmail.com>
2023-10-19 10:00:23 +0300

    Audio Update

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
+import requests
 from flask import Flask, request, jsonify
 from utils import prompts, gpt3, stable_diffusion
 from google.cloud import firestore
+from keys import eleven_key
+from elevenlabs import set_api_key
 
 app = Flask(__name__)
 
 db = firestore.Client(project='a2sv-hackathon')
+set_api_key(eleven_key)
+CHUNK_SIZE = 1024
+speech_url = "https://api.elevenlabs.io/v1/text-to-speech/21m00Tcm4TlvDq8ikWAM"
 
 @app.route('/', methods=['GET'])
 def test():
@@ -46,9 +52,9 @@ def generate_story():
                     print(e)
 
         # Combine the story with images
+        img_list = []
         parts = story.split("\n\n")
         parts = [part for part in parts if len(part) > 0]
-
         story_with_images = ""
         for i, part in enumerate(parts):
             if "replicate.com" not in part:
@@ -61,6 +67,7 @@ def generate_story():
                 try:
                     image_prompt = prompts.illustration(f"{plot}\n\n{'' if i == 0 else parts[i - 1]}\n\n{part}")
                     image_url = stable_diffusion.generate_image(image_prompt)
+                    img_list.append(image_url)
                     story_with_images += image_url + "\n\n"
                 except Exception as e:
                     print(e)
@@ -77,9 +84,34 @@ def generate_story():
 
         response = {
             'status': 'success',
-            'uid': name,
+            'uid': uid,
             'story': story_with_images,
+            'parts': parts,
+            'images': img_list
         }
+        
+        headers = {
+            "Accept": "audio/mpeg",
+            "Content-Type": "application/json",
+            "xi-api-key": eleven_key
+        }
+        
+        audio_data = {
+            "text": story.strip("\n\n"),
+            "model_id": "eleven_monolingual_v1",
+            "voice_settings": {
+            "stability": 0.5,
+            "similarity_boost": 0.5
+                }
+            }
+        
+        audio_response = requests.post(speech_url, json=audio_data, headers=headers)
+        
+        with open('output.mp3', 'wb') as f:
+            for chunk in audio_response.iter_content(chunk_size=CHUNK_SIZE):
+             if chunk:
+                f.write(chunk)
+        
 
         return jsonify(response)
     except Exception as e:
