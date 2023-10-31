0db8191edaffa2e1c002b63abd2711bc415d470b
Wayne Asava <97846348+AsavaAsava@users.noreply.github.com>
2023-11-01 01:59:06 +0300

    Removed API keys

--- utils/keys.py
+++
@@ -1,4 +0,0 @@
-eleven_key = "41ea805b26514d2cb0c684ca8e383cde"
-OPENAI_API_KEY = "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"
-REPLICATE_API_TOKEN = "r8_cD2iYmXt4aoBkPGFS3e0sweOQYVm9UH48yOrZ"
-GOOGLE_APPLICATION_CREDENTIALS = r"C:\Users\Nathan\Downloads\service-creds.json"
